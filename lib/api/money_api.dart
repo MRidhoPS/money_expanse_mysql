3b202a260ac26c0d4ffead31d236a8842415c6d3
MRidhoPS <mridhophs@gmail.com>
2024-05-30 18:14:52 +0700

    project register account done

--- lib/api/money_api.dart
+++ lib/api/money_api.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -8,14 +10,16 @@ class ApiService {
   final configapp = config_api.ApiConfig();
 
   Future<void> registerAccount(Money money) async {
-    final response = await http.post(Uri.parse(configapp.registerUrl),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(money.toJson()));
+    final response = await http.post(
+      Uri.parse('${configapp.baseUrl}/register'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(money.toJson()),
+    );
 
     if (response.statusCode != 201) {
-      throw Exception('Failed to registered account');
+      throw Exception('Failed Regist Account student');
     }
   }
 }
